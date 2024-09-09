ae2a6b1e975c46c1004c95acfcf1dbaf5a1b2458
Omed Mehdizada <omed@konnektiv.de>
2024-09-09 08:56:25 +0200

    progress

--- src/main.c
+++ src/main.c
@@ -6,11 +6,13 @@
 /*   By: omed <omed@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 16:11:14 by omed              #+#    #+#             */
-/*   Updated: 2024/09/06 17:54:15 by omed             ###   ########.fr       */
+/*   Updated: 2024/09/07 16:50:55 by omed             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minilibx-linux/mlx.h"
+
+#define ESC_KEY 65307
 
 typedef struct	s_data {
 	void	*img;
@@ -20,6 +22,12 @@ typedef struct	s_data {
 	int		endian;
 }				t_data;
 
+
+typedef struct  s_vars {
+    void *mlx;
+    void *win;
+} t_vars;
+
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
@@ -27,16 +35,22 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
 }
+int close(int keycode, t_vars *vars)
+{
+    if (keycode == ESC_KEY)
+        mlx_destroy_window(vars->mlx, vars->win);
+    return(0);
+}
+
 
 int main()
 {
-    void    *mlx;
-    void    *mlx_win;
     t_data  img;
+    t_vars  vars;
 
-    mlx = mlx_init();
-    mlx_win = mlx_new_window(mlx, 1000, 2080, "Hello Omed!");
-    img.img = mlx_new_image(mlx, 640, 360);
+    vars.mlx = mlx_init();
+    vars.win = mlx_new_window(vars.mlx, 1000, 2080, "Hello Omed!");
+    img.img = mlx_new_image(vars.mlx, 640, 360);
     img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
     
     int x = 10;
@@ -47,11 +61,12 @@ int main()
     {
         my_mlx_pixel_put(&img, x, y, 0x00FF0000);
         my_mlx_pixel_put(&img, a, b, 0x00FF0000);
-	    mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	    mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
         x++;
         y++;
         a--;
         b++;
     }
-    mlx_loop(mlx);
+    mlx_hook(vars.win, 2, 1L<<0, close, &vars);
+    mlx_loop(vars.mlx);
 }
